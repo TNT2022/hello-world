4e0ea9c7b86c332f848eeb24f4200db0e8344967
EFZR <emerson.zapatarivas@gmail.com>
2022-02-14 08:47:27 -0600

    first commit

---
+++ hello_world.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x=5;
+    echo '<h1 style="text-align: center;">Hello World!</h1>';
+    echo $x;
+    ?>
+
+    <table border = "1px solid black">
+        <thead>
+            <tr>
+                <th>Emerson</th>
+                <th>Feranando</th>
+                <th>Zapata</th>
+                <th>Rivas</th>
+            </tr>
+        </thead>
+    </table>
+</body>
+</html>
